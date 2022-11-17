21f38334f9ae91e6bdb0e19ceb63e4af45e2360a
Mateus-A-R-Braga <mateus.braga@colorado.edu>
2022-11-17 07:50:34 -0700

    Removing unused parameters and simplifying to have only velocity products required for Reynolds stress

--- examples/fluids/qfunctions/reynolds_stress.h
+++ examples/fluids/qfunctions/reynolds_stress.h
@@ -19,29 +19,23 @@
 
 CEED_QFUNCTION_HELPER int ReynoldsStress(void *ctx, CeedInt Q,
     const CeedScalar *const *in, CeedScalar *const *out,
-    StateFromQi_t StateFromQi, StateFromQi_fwd_t StateFromQi_fwd) {
+    StateFromQi_t StateFromQi) {
+
   // *INDENT-OFF*
   // Inputs
   const CeedScalar (*q)[CEED_Q_VLA]         = (const CeedScalar(*)[CEED_Q_VLA])in[0],
-                   (*Grad_q)[5][CEED_Q_VLA] = (const CeedScalar(*)[5][CEED_Q_VLA])in[1],
-                   (*q_data)[CEED_Q_VLA]    = (const CeedScalar(*)[CEED_Q_VLA])in[2],
-                   (*x)[CEED_Q_VLA]         = (const CeedScalar(*)[CEED_Q_VLA])in[3];
-  // Grad_q not needed for this example
-
+                   (*q_data)[CEED_Q_VLA]    = (const CeedScalar(*)[CEED_Q_VLA])in[1],
+                   (*x)[CEED_Q_VLA]         = (const CeedScalar(*)[CEED_Q_VLA])in[2];
 
   // Outputs
-  CeedScalar (*Grad_v)[5][CEED_Q_VLA] = (CeedScalar(*)[5][CEED_Q_VLA])out[0],
-             (*jac_data)[CEED_Q_VLA]  = (CeedScalar(*)[CEED_Q_VLA])out[1];
-
-  // Outputs should include the product of U_iU_j
-
+  CeedScalar (*U_prod)[6][CEED_Q_VLA] = (CeedScalar(*)[6][CEED_Q_VLA])out[0];
 
   // *INDENT-ON*
   // Context
   const NewtonianIdealGasContext context        = (NewtonianIdealGasContext)ctx;
   const CeedScalar dt                           = context->dt;
-  const StateConservative ZeroFlux              = {0., {0., 0., 0.}, 0.};
-  const StateConservative ZeroInviscidFluxes[3] = {ZeroFlux, ZeroFlux, ZeroFlux};
+//  const StateConservative ZeroFlux              = {0., {0., 0., 0.}, 0.};
+//  const StateConservative ZeroInviscidFluxes[3] = {ZeroFlux, ZeroFlux, ZeroFlux};
 
   CeedPragmaSIMD
   // Quadrature Point Loop
@@ -55,62 +49,76 @@ CEED_QFUNCTION_HELPER int ReynoldsStress(void *ctx, CeedInt Q,
     // -- Interp-to-Grad q_data
     // ---- Inverse of change of coordinate matrix: X_i,j
     // *INDENT-OFF*
-    const CeedScalar dXdx[3][3] = {{q_data[1][i], q_data[2][i], q_data[3][i]},
-                                   {q_data[4][i], q_data[5][i], q_data[6][i]},
-                                   {q_data[7][i], q_data[8][i], q_data[9][i]}
-                                  };
+//    const CeedScalar dXdx[3][3] = {{q_data[1][i], q_data[2][i], q_data[3][i]},
+//                                   {q_data[4][i], q_data[5][i], q_data[6][i]},
+//                                   {q_data[7][i], q_data[8][i], q_data[9][i]}
+//                                  };
+
+    CeedScalar U_prod[6];
+   // Using Voight notation (array ordering)
+    U_prod[0] = qi[1] * qi[1] * wdetJ // U*U
+    U_prod[1] = qi[2] * qi[2] * wdetJ // V*V
+    U_prod[2] = qi[3] * qi[3] * wdetJ // W*W
+    U_prod[3] = qi[2] * qi[3] * wdetJ // V*W
+    U_prod[4] = qi[1] * qi[3] * wdetJ // U*W
+    U_prod[5] = qi[1] * qi[2] * wdetJ // U*V
+    
+   // Where to multiply wdetJ ?
+
+
+
     // *INDENT-ON*
-    State grad_s[3];
-    for (CeedInt j=0; j<3; j++) {
-      CeedScalar dx_i[3] = {0}, dqi[5];
-      for (CeedInt k=0; k<5; k++)
-        dqi[k] = Grad_q[0][k][i] * dXdx[0][j] +
-                 Grad_q[1][k][i] * dXdx[1][j] +
-                 Grad_q[2][k][i] * dXdx[2][j];
-      dx_i[j] = 1.;
-      grad_s[j] = StateFromQi_fwd(context, s, dqi, x_i, dx_i);
-    }
+//    State grad_s[3];
+//    for (CeedInt j=0; j<3; j++) {
+//      CeedScalar dx_i[3] = {0}, dqi[5];
+//      for (CeedInt k=0; k<5; k++)
+//        dqi[k] = Grad_q[0][k][i] * dXdx[0][j] +
+//                 Grad_q[1][k][i] * dXdx[1][j] +
+//                 Grad_q[2][k][i] * dXdx[2][j];
+//      dx_i[j] = 1.;
+//      grad_s[j] = StateFromQi_fwd(context, s, dqi, x_i, dx_i);
+//    }
 
-
-// HERE DOWN UPDATE TO BE UNIQUE FOR REYNOLDS STRESSES
-    CeedScalar strain_rate[6], kmstress[6], stress[3][3], Fe[3];
-    KMStrainRate(grad_s, strain_rate);
-    NewtonianStress(context, strain_rate, kmstress);
-    KMUnpack(kmstress, stress);
-    ViscousEnergyFlux(context, s.Y, grad_s, stress, Fe);
+//    CeedScalar strain_rate[6], kmstress[6], stress[3][3], Fe[3];
+//    KMStrainRate(grad_s, strain_rate);
+//    NewtonianStress(context, strain_rate, kmstress);
+//    KMUnpack(kmstress, stress);
+//    ViscousEnergyFlux(context, s.Y, grad_s, stress, Fe);
 
     // Total flux
-    CeedScalar DiffFlux[5][3];
-    FluxTotal(ZeroInviscidFluxes, stress, Fe, DiffFlux);
+//    CeedScalar DiffFlux[5][3];
+//    FluxTotal(ZeroInviscidFluxes, stress, Fe, DiffFlux);
 
-    for (CeedInt j=0; j<3; j++) {
-      for (CeedInt k=0; k<5; k++) {
-        Grad_v[j][k][i] = -wdetJ * (dXdx[j][0] * DiffFlux[k][0] +
-                                    dXdx[j][1] * DiffFlux[k][1] +
-                                    dXdx[j][2] * DiffFlux[k][2]);
-      }
-    }
+//    for (CeedInt j=0; j<3; j++) {
+//      for (CeedInt k=0; k<5; k++) {
+//        Grad_v[j][k][i] = -wdetJ * (dXdx[j][0] * DiffFlux[k][0] +
+//                                    dXdx[j][1] * DiffFlux[k][1] +
+//                                    dXdx[j][2] * DiffFlux[k][2]);
+//      }
+//    }
 
-    CeedScalar Tau_d[5];
-    Tau_diagPrim(context, s, dXdx, dt, Tau_d);
+//    CeedScalar Tau_d[5];
+//    Tau_diagPrim(context, s, dXdx, dt, Tau_d);
 
-    for (CeedInt j=0; j<5; j++) jac_data[j][i]     = qi[j];
-    for (CeedInt j=0; j<6; j++) jac_data[5+j][i]   = kmstress[j];
-    for (CeedInt j=0; j<3; j++) jac_data[5+6+j][i] = Tau_d[j];
+
+
+
+
+
   } // End Quadrature Point Loop
 
   // Return
   return 0;
 }
 
-CEED_QFUNCTION(DivDiffusiveFlux_Conserv)(void *ctx, CeedInt Q,
+CEED_QFUNCTION(ReynoldsStress_Conserv)(void *ctx, CeedInt Q,
     const CeedScalar *const *in, CeedScalar *const *out) {
-  return DivDiffusiveFlux(ctx, Q, in, out, StateFromU, StateFromU_fwd);
+  return ReynoldsStress(ctx, Q, in, out, StateFromU);
 }
 
-CEED_QFUNCTION(DivDiffusiveFlux_Prim)(void *ctx, CeedInt Q,
-                                      const CeedScalar *const *in, CeedScalar *const *out) {
-  return DivDiffusiveFlux(ctx, Q, in, out, StateFromY, StateFromY_fwd);
+CEED_QFUNCTION(ReynoldsStress_Prim)(void *ctx, CeedInt Q,
+    const CeedScalar *const *in, CeedScalar *const *out) {
+  return ReynoldsStress(ctx, Q, in, out, StateFromY);
 }
 
 #endif // reynolds_stress_h
